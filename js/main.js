1c0932feb6bff8cae8583dc3589683013acb4ad0
Harshit Seksaria <37345795+Zlytherin@users.noreply.github.com>
2019-01-13 10:26:27 -0800

    Added intro and skills

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ const myWorld = $('#myWorld');
 const equal = $('#equal');
 const code = $('#code');
 const colon = $('#colon');
+const progList = $('#prog-list');
+const markList = $('#mark-list');
+const libList = $('#lib-list');
+const fireList = $('#fire-list')
  
 const margin = `${header.outerHeight() + 10}px`;
 wrapper.css('marginTop', margin);
@@ -21,3 +25,45 @@ setTimeout(() => showHeroText(myWorld, "myWorld", 500),0);
 setTimeout(() => showHeroText(equal, " = ", 500), 4000);
 setTimeout(() => showHeroText(code, "code", 500), 5800);
 setTimeout(() => colon.text(';'), 8000)
+
+const removeFocus = () => {
+    progList.slideUp();
+    markList.slideUp();
+    libList.slideUp();
+    fireList.slideUp();
+    $('#prog').removeClass('active');
+    $('#mark').removeClass('active');
+    $('#lib').removeClass('active');
+    $('#fire').removeClass('active');
+}
+
+removeFocus();
+
+$('#prog').on('click', () => {
+    removeFocus();
+    if (!progList.is(':visible')) {
+        $('#prog').addClass('active');
+        progList.slideDown();
+    }
+});
+$('#mark').on('click', () => {
+    removeFocus();
+    if (!markList.is(':visible')) {
+        $('#mark').addClass('active');
+        markList.slideDown();
+    }
+});
+$('#lib').on('click', () => {
+    removeFocus();
+    if (!libList.is(':visible')) {
+        $('#lib').addClass('active');
+        libList.slideDown();
+    }
+});
+$('#fire').on('click', () => {
+    removeFocus();
+    if (!fireList.is(':visible')) {
+        $('#fire').addClass('active');
+        fireList.slideDown();
+    }
+});
